9dd98cf918d53511a283923c34588ce686d33559
영효 심 <dark1203@naver.com>
2022-05-19 13:02:38 +0900

    init

---
+++ src/main/java/com/ssafy/happyhouse/model/DongDto.java
@@ -0,0 +1,35 @@
+package com.ssafy.happyhouse.model;
+
+public class DongDto {
+	String dongCode;
+	String sidoName;
+	String gugunName;
+	String dongName;
+	
+	public String getDongCode() {
+		return dongCode;
+	}
+	public void setDongCode(String dongCode) {
+		this.dongCode = dongCode;
+	}
+	public String getSidoName() {
+		return sidoName;
+	}
+	public void setSidoName(String sidoName) {
+		this.sidoName = sidoName;
+	}
+	public String getGugunName() {
+		return gugunName;
+	}
+	public void setGugunName(String gugunName) {
+		this.gugunName = gugunName;
+	}
+	public String getDongName() {
+		return dongName;
+	}
+	public void setDongName(String dongName) {
+		this.dongName = dongName;
+	}
+	
+	
+}
---
+++ src/main/java/com/ssafy/happyhouse/model/GugunDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.happyhouse.model;
+
+public class GugunDto {
+	String gugunCode;
+	String gugunName;
+	
+	public String getGugunCode() {
+		return gugunCode;
+	}
+	public void setGugunCode(String gugunCode) {
+		this.gugunCode = gugunCode;
+	}
+	public String getGugunName() {
+		return gugunName;
+	}
+	public void setGugunName(String gugunName) {
+		this.gugunName = gugunName;
+	}
+	
+}
