32f9c024c858c0f06ff9f303766431fe74b20634
yb8350 <yb8350@naver.com>
2022-05-23 11:30:44 +0900

    Update board

--- src/main/java/com/ssafy/happyhouse/controller/BoardController.java
+++ src/main/java/com/ssafy/happyhouse/controller/BoardController.java
@@ -10,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -106,5 +110,50 @@ public class BoardController {
 		}
 
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+    @ApiOperation(value = "게시판 글등록", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PostMapping("/write")
+	public ResponseEntity<String> writeBoard(@RequestBody BoardDto board) {
+		logger.debug("writeBoard - 호출");
+		try {
+			if (boardService.registerArticle(board)) {
+				System.out.println("???");
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+	@ApiOperation(value = "게시판 글정보 수정", notes = "글번호에 해당하는 게시글의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PutMapping("/detail/{articleno}")
+	public ResponseEntity<String> updateBoard(@RequestBody BoardDto board) {
+		logger.debug("updateBoard - 호출");
+		
+		try {
+			if (boardService.updateArticle(board)) {
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+    @ApiOperation(value = "게시판 글삭제", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@DeleteMapping("/detail/{articleno}")
+	public ResponseEntity<String> deleteBoard(@PathVariable int articleno) {
+		logger.debug("deleteBoard - 호출");
+		try {
+			if (boardService.deleteArticle(articleno)) {
+				return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 }
